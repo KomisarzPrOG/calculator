84af7bc4e2b94c534bc4bf695e413a8ce480ba56
KomisarzPrOGKomp <ja123@gmail.com>
2025-02-08 19:00:41 +0100

    Add calculate() and fix some bugs

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 const OUTPUT = document.getElementById('output-wynik')
 var lastComa = false
+var lastOper = true
 
 function addVar(war) {
     var outputContent = OUTPUT.innerText
 
-    if(lastComa == false && war == '.') {
+    if(outputContent == "" && war == '.' && lastComa == false || lastOper == true && war == '.' && lastComa == false) {
+        OUTPUT.innerText = outputContent + "0" + war
+        lastComa = true
+        lastOper = true
+    } else if(lastComa == false && war == '.') {
         OUTPUT.innerText = outputContent + war
         lastComa = true
-    } else if(war != '.') {
+    } else if(lastOper == false && war == '+' || lastOper == false && war == '-' || lastOper == false && war == '*' || lastOper == false && war == '/') {
         OUTPUT.innerText = outputContent + war
+    } else if(war != '.' && war != '+' && war != '-' && war != '*' && war != '/') {
+        OUTPUT.innerText = outputContent + war
+        lastOper = false
     }
     if(war == '+' || war == "-" || war == '*' || war == '/') {
         lastComa = false
+        lastOper = true
     }
 }
 
 function clearAll() {
     OUTPUT.innerText = ""
+    lastComa = false
+    lastOper = true
+}
+
+function calculate() {
+    outputContent = OUTPUT.innerText
+    let lastWar = outputContent.slice(-1)
+    
+    if(outputContent != "") {
+        if(!['+','-','*','/'].includes(lastWar)) {
+            var result = eval(outputContent)
+        }
+    }
+
+    OUTPUT.innerText = result
 }
 
 // TODO: add calculate, clearLast, and addPercentage functions
