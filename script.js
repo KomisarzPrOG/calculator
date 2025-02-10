9f9c910fa0f7296be5e9063e53d7a3b298a32f24
KomisarzPrOGKomp <ja123@gmail.com>
2025-02-10 18:13:56 +0100

    Add clearLast function

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const OUTPUT = document.getElementById('output-wynik')
 var lastComa = false
 var lastOper = true
+var outputContent
 
 function addVar(war) {
-    var outputContent = OUTPUT.innerText
+    outputContent = OUTPUT.innerText
 
     if(outputContent == "" && war == '.' && lastComa == false || lastOper == true && war == '.' && lastComa == false) {
         OUTPUT.innerText = outputContent + "0" + war
@@ -30,21 +31,51 @@ function clearAll() {
     lastOper = true
 }
 
+function clearLast() {
+    outputContent = OUTPUT.innerText
+
+    if(outputContent != "") {
+        if(lastComa == false) {
+            if(lastOper == false) {
+                outputContent = outputContent.slice(0,-1)
+            } else {
+                outputContent = outputContent.slice(0,-1)
+                lastOper = false
+            }
+        } else if(outputContent.slice(-2) == 0) {
+            outputContent = outputContent.slice(0,-2)
+            lastComa = false
+        } else {
+            outputContent = outputContent.slice(0,-1) 
+        }
+    }
+
+    if(['+','-','*','/'].includes(outputContent.slice(-1))) {
+        lastOper = true
+    }
+
+    OUTPUT.innerText = outputContent
+}
+
 function calculate() {
     outputContent = OUTPUT.innerText
     let lastWar = outputContent.slice(-1)
     
     if(outputContent != "") {
-        if(!['+','-','*','/'].includes(lastWar)) {
-            var result = eval(outputContent)
-        } else {
-            throw "Last var was an operator!"
-        }
+            if(!['+','-','*','/'].includes(lastWar)) {
+                var result = eval(outputContent)
+            } else {
+                throw "Last var was an operator!"
+            }
     } else {
         result = ""
     }
 
-    OUTPUT.innerText = result
+    if(!isFinite(result)) {
+        OUTPUT.innerText = "0"
+    } else{
+       OUTPUT.innerText = result 
+    }    
 }
 
-// TODO: clearLast, and addPercentage functions
+// TODO: clearLast and addPercentage functions
